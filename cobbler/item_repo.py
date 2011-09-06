3fba0e4338de7ee61dcc7256c3a5dcd6eb24be5f
Javier Palacios <javiplx@gmail.com>
2011-09-06 18:22:52 +0200

    REFACTORING : move sync method into proper Repo-derived objects

--- cobbler/item_repo.py
+++ cobbler/item_repo.py
@@ -24,7 +24,7 @@ import utils
 import item
 from cexceptions import *
 from utils import _
-import time
+import os
 
 VALID_REPO_BREEDS = ( "rsync", "rhn", "yum", "apt" )
 
@@ -72,6 +72,9 @@ class Repo(item.Item):
         obj.from_datastruct(seed_data)
         return obj
     Factory = staticmethod(Factory)
+
+    def sync(self, logger):
+        utils.die(logger,"unable to sync repo (%s), unknown or unsupported repo type (%s)" % (self.name, self.breed))
 
     def make_clone(self):
         ds = self.to_datastruct()
@@ -208,25 +211,317 @@ class VoidRepo ( Repo ) :
 
     def guess_breed(self,mirror):
         if mirror.startswith("http://") or mirror.startswith("ftp://"):
-             return "yum"
-         elif mirror.startswith("rhn://"):
-             return "rhn"
-         else:
-             return "rsync"
+            return "yum"
+        elif mirror.startswith("rhn://"):
+            return "rhn"
+        else:
+            return "rsync"
 
-class RsyncRepo ( Repo ) :
+class _RpmRepo ( Repo ) :
+
+    def create_local_file(self, dest_path, logger, output=True):
+        """
+        Creates Yum config files for use by reposync
+
+        Two uses:
+        (A) output=True, Create local files that can be used with yum on provisioned clients to make use of this mirror.
+        (B) output=False, Create a temporary file for yum to feed into yum for mirroring
+        """
+    
+        # the output case will generate repo configuration files which are usable
+        # for the installed systems.  They need to be made compatible with --server-override
+        # which means they are actually templates, which need to be rendered by a cobbler-sync
+        # on per profile/system basis.
+
+        if output:
+            fname = os.path.join(dest_path,"config.repo")
+        else:
+            fname = os.path.join(dest_path, "%s.repo" % self.name)
+        logger.debug("creating: %s" % fname)
+        if not os.path.exists(dest_path):
+            utils.mkdir(dest_path)
+        config_file = open(fname, "w+")
+        config_file.write("[%s]\n" % self.name)
+        config_file.write("name=%s\n" % self.name)
+        optenabled = False
+        optgpgcheck = False
+        if output:
+            line = "baseurl=http://${server}/cobbler/repo_mirror/%s\n" % self.name
+  
+            config_file.write(line)
+            # user may have options specific to certain yum plugins
+            # add them to the file
+            for x in self.yumopts:
+                config_file.write("%s=%s\n" % (x, self.yumopts[x]))
+                if x == "enabled":
+                    optenabled = True
+                if x == "gpgcheck":
+                    optgpgcheck = True
+        else:
+            mstr = self.mirror
+            if mstr.startswith("/"):
+                mstr = "file://%s" % mstr
+            line = "baseurl=%s\n" % mstr
+            if self.settings.http_port not in (80, '80'):
+                http_server = "%s:%s" % (self.settings.server, self.settings.http_port)
+            else:
+                http_server = self.settings.server
+            line = line.replace("@@server@@",http_server)
+            config_file.write(line)
+        if not optenabled:
+            config_file.write("enabled=1\n")
+        config_file.write("priority=%s\n" % self.priority)
+        # FIXME: potentially might want a way to turn this on/off on a per-repo basis
+        if not optgpgcheck:
+            config_file.write("gpgcheck=0\n")
+        config_file.close()
+        return fname 
+
+    def createrepo_walker(self, logger, dirname, fnames):
+        """
+        Used to run createrepo on a copied Yum mirror.
+        """
+        if os.path.exists(dirname) or self['breed'] == 'rsync':
+            utils.remove_yum_olddata(dirname)
+
+            # add any repo metadata we can use
+            mdoptions = []
+            if os.path.isfile("%s/.origin/repomd.xml" % (dirname)):
+                if not HAS_YUM:
+                   utils.die(logger,"yum is required to use this feature")
+
+                rmd = yum.repoMDObject.RepoMD('', "%s/.origin/repomd.xml" % (dirname))
+                if rmd.repoData.has_key("group"):
+                    groupmdfile = rmd.getData("group").location[1]
+                    mdoptions.append("-g %s" % groupmdfile)
+                if rmd.repoData.has_key("prestodelta"):
+                    # need createrepo >= 0.9.7 to add deltas
+                    if utils.check_dist() == "redhat" or utils.check_dist() == "suse":
+                        cmd = "/usr/bin/rpmquery --queryformat=%{VERSION} createrepo"
+                        createrepo_ver = utils.subprocess_get(logger, cmd)
+                        if createrepo_ver >= "0.9.7":
+                            mdoptions.append("--deltas")
+                        else:
+                            logger.error("this repo has presto metadata; you must upgrade createrepo to >= 0.9.7 first and then need to resync the repo through cobbler.")
+
+            blended = utils.blender(self.config.api, False, self)
+            flags = blended.get("createrepo_flags","(ERROR: FLAGS)")
+            try:
+                # BOOKMARK
+                cmd = "createrepo %s %s %s" % (" ".join(mdoptions), flags, dirname)
+                utils.subprocess_call(logger, cmd)
+            except:
+                utils.log_exc(logger)
+                logger.error("createrepo failed.")
+            del fnames[:] # we're in the right place
+
+class RsyncRepo ( _RpmRepo ) :
 
     breed = "rsync"
 
-class YumRepo ( Repo ) :
+    def sync(self, logger):
+
+        if self.rpm_list :
+            logger.warning("--rpm-list is not supported for rsync'd repositories")
+
+        # FIXME: don't hardcode
+        dest_path = os.path.join(self.settings.webdir+"/repo_mirror", self.name)
+
+        # FIXME : check any interference due introduced by utils function, which preserve permission & owner
+        if not utils.rsync_files( self.mirror , dest_path ,  "--verbose --delete" , logger ) :
+            utils.die(logger,"cobbler reposync failed")
+
+        os.path.walk(dest_path, self.createrepo_walker, logger)
+        self.create_local_file(dest_path, logger)
+
+class YumRepo ( _RpmRepo ) :
 
     breed = "yum"
 
-class RhnRepo ( Repo ) :
+    def sync(self, logger):
+
+        if not os.path.exists("/usr/bin/reposync"):
+            utils.die(logger,"no /usr/bin/reposync found, please install yum-utils")
+
+        # create yum config file for use by reposync
+        dest_path = os.path.join(self.settings.webdir+"/repo_mirror", self.name)
+        temp_path = os.path.join(dest_path, ".origin")
+
+        if not os.path.isdir(temp_path):
+            # FIXME: there's a chance this might break the RHN D/L case
+            os.makedirs(temp_path)
+         
+        # create the config file that yum will use for the copying
+
+        temp_file = self.create_local_file(temp_path, logger, False)
+
+        if not self.rpm_list:
+            # if we have not requested only certain RPMs, use reposync
+            rflags = self.settings.reposync_flags
+            cmd = "/usr/bin/reposync %s --config=%s --repoid=%s --download_path=%s" % (rflags, temp_file, self.name, self.settings.webdir+"/repo_mirror")
+            if self.arch != "":
+                if self.arch == "x86":
+                   self.arch = "i386" # FIX potential arch errors
+                if self.arch == "i386":
+                   # counter-intuitive, but we want the newish kernels too
+                   cmd = "%s -a i686" % (cmd)
+                else:
+                   cmd = "%s -a %s" % (cmd, self.arch)
+
+        else:
+
+            # create the output directory if it doesn't exist
+            if not os.path.exists(dest_path):
+               os.makedirs(dest_path)
+
+            use_source = ""
+            if self.arch == "src":
+                use_source = "--source"
+ 
+            # older yumdownloader sometimes explodes on --resolvedeps
+            # if this happens to you, upgrade yum & yum-utils
+            extra_flags = self.settings.yumdownloader_flags
+            cmd = "/usr/bin/yumdownloader %s %s --disablerepo=* --enablerepo=%s -c %s --destdir=%s %s" % (extra_flags, use_source, self.name, temp_file, dest_path, " ".join(self.rpm_list))
+
+        rc = utils.subprocess_call(logger, cmd)
+        if rc !=0:
+            utils.die(logger,"cobbler reposync failed")
+
+        repodata_path = os.path.join(dest_path, "repodata")
+
+        if not os.path.exists("/usr/bin/wget"):
+            utils.die(logger,"no /usr/bin/wget found, please install wget")
+
+        # grab repomd.xml and use it to download any metadata we can use
+        cmd2 = "/usr/bin/wget -q %s/repodata/repomd.xml -O %s/repomd.xml" % (self.mirror, temp_path)
+        rc = utils.subprocess_call(logger,cmd2)
+        if rc == 0:
+            # create our repodata directory now, as any extra metadata we're
+            # about to download probably lives there
+            if not os.path.isdir(repodata_path):
+                os.makedirs(repodata_path)
+            rmd = yum.repoMDObject.RepoMD('', "%s/repomd.xml" % (temp_path))
+            for mdtype in rmd.repoData.keys():
+                # don't download metadata files that are created by default
+                if mdtype not in ["primary", "primary_db", "filelists", "filelists_db", "other", "other_db"]:
+                    mdfile = rmd.getData(mdtype).location[1]
+                    cmd3 = "/usr/bin/wget -q %s/%s -O %s/%s" % (self.mirror, mdfile, dest_path, mdfile)
+                    utils.subprocess_call(logger,cmd3)
+                    if rc !=0:
+                        utils.die(logger,"wget failed")
+
+        # now run createrepo to rebuild the index
+        os.path.walk(dest_path, self.createrepo_walker, logger)
+
+        # create the config file the hosts will use to access the repository.
+        self.create_local_file(dest_path, logger)
+
+class RhnRepo ( _RpmRepo ) :
 
     breed = "rhn"
+
+    def sync(self, logger):
+
+        if self.rpm_list:
+            logger.warning("--rpm-list is not supported for RHN content")
+
+        if not os.path.exists("/usr/bin/reposync"):
+            utils.die(logger,"no /usr/bin/reposync found, please install yum-utils")
+
+        # create yum config file for use by reposync
+        # FIXME: don't hardcode
+        dest_path = os.path.join(self.settings.webdir+"/repo_mirror", self.name)
+        temp_path = os.path.join(dest_path, ".origin")
+
+        if not os.path.isdir(temp_path):
+            # FIXME: there's a chance this might break the RHN D/L case
+            os.makedirs(temp_path)
+         
+        rest = self.mirror[6:] # everything after rhn://
+        rflags = self.settings.reposync_flags
+        cmd = "/usr/bin/reposync %s -r %s --download_path=%s" % (rflags, rest, self.settings.webdir+"/repo_mirror")
+        if self.name != rest:
+            args = { "name" : self.name, "rest" : rest }
+            utils.die(logger,"ERROR: repository %(name)s needs to be renamed %(rest)s as the name of the cobbler repository must match the name of the RHN channel" % args)
+
+        if self.arch == "i386":
+            # counter-intuitive, but we want the newish kernels too
+            self.arch = "i686"
+
+        if self.arch != "":
+            cmd = "%s -a %s" % (cmd, self.arch)
+
+        # Don't die if reposync fails, it is logged
+        utils.subprocess_call(logger, cmd)
+
+        # some more special case handling for RHN.
+        # create the config file now, because the directory didn't exist earlier
+
+        temp_file = self.create_local_file(temp_path, logger, False)
+
+        # now run createrepo to rebuild the index
+
+        os.path.walk(dest_path, self.createrepo_walker, logger)
+
+        # create the config file the hosts will use to access the repository.
+
+        self.create_local_file(dest_path, logger)
 
 class AptRepo ( Repo ) :
 
     breed = "apt"
+
+    def sync(self, logger):
+
+        if self.rpm_list :
+            logger.warning("--rpm-list not yet supported on apt repos")
+
+        mirror_program = "/usr/bin/debmirror"
+        if not os.path.exists(mirror_program):
+            utils.die(logger,"no %s found, please install it"%(mirror_program))
+
+        if not self.arch:
+            utils.die(logger,"Architecture is required for apt repositories")
+
+        # built destination path for the repo
+        dest_path = os.path.join("/var/www/cobbler/repo_mirror", self.name)
+         
+        mirror = self.mirror.replace("@@suite@@",self.os_version)
+
+        idx = mirror.find("://")
+        method = mirror[:idx]
+        mirror = mirror[idx+3:]
+
+        idx = mirror.find("/")
+        host = mirror[:idx]
+        mirror = mirror[idx+1:]
+
+        idx = mirror.rfind("/dists/")
+        suite = mirror[idx+7:]
+        mirror = mirror[:idx]
+
+        mirror_data = "--method=%s --host=%s --root=%s --dist=%s " % ( method , host , mirror , suite )
+
+        # FIXME : flags should come from repo instead of being hardcoded
+
+        rflags = "--passive --nocleanup"
+        for x in self.yumopts:
+            if self.yumopts[x]:
+                rflags += " %s %s" % ( x , self.yumopts[x] ) 
+            else:
+                rflags += " %s" % x 
+        cmd = "%s %s %s %s" % (mirror_program, rflags, mirror_data, dest_path)
+        if self.arch == "src":
+            cmd = "%s --source" % cmd
+        else:
+            arch = self.arch
+            if arch == "x86":
+               arch = "i386" # FIX potential arch errors
+            if arch == "x86_64":
+               arch = "amd64" # FIX potential arch errors
+            cmd = "%s --nosource -a %s" % (cmd, arch)
+                    
+        rc = utils.subprocess_call(logger, cmd)
+        if rc !=0:
+            utils.die(logger,"cobbler reposync failed")
 
